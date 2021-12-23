574eb6a2e056d3ae943ec4df014844a62a686978
Dillon Wu <dillonw@andrew.cmu.edu>
2021-12-23 04:14:24 -0500

    general ssh

--- cryptohacks/general/general.py
+++ cryptohacks/general/general.py
@@ -70,7 +70,7 @@ def lemur_xor():
 	plt.imsave('/Users/Kvothe/Downloads/lemur_flag.png', lemur_flag.astype(np.uint8))
 
 def main():
-
+	
 	### Solutions: Encoding
 	print('--- ENCODING ---')
 	# 1
@@ -208,6 +208,27 @@ def main():
 	n = cert.public_key.native["public_key"]["modulus"]
 
 	print(n)
+
+	# 3
+	# from openssh -> openssl, the command is:
+	# ssh-keygen -f ~/Downloads/bruce_rsa_6e7ecd53b443a97013397b1a1ea30e14.pub -e -m pem
+	pub_key = '''
+	-----BEGIN RSA PUBLIC KEY-----
+MIIBigKCAYEArTy6m2vhhbwx3RVbNVb3ZOenCqqsOXHaJpbtN+OuulLKBSKpIoPB
++ZDbDXn0qWkf4lOxtGSgolkUbgG07Lhzfgs+dul4UL84CkwZExmF3Rf1nRv+v7pq
+Lt2dPsCb02YLxJnhHJb4rQaz2ZM4QCtTOcqYDUeKfLHCaZU4Ekm/OApKrpfw4/0o
+fn8KOrFN0t4/dqnNuwVRgoaUIhsI47reApB2rs0AP4CggSIi8s6BXCxB4YzgThBK
+5760T1giACYQC5MFdq1Gw+INSFmu0CNqt5wdJ5Z4z5448Gke06R+IMtjUiGDQ3Qt
+T2fK3gWhZxk14M4UNrdETgTW/mQ4B/BcvikxvoBGpKbttG0agfOjTen6wyzpGfcd
+8N9rSbaqqyUwC8uDotzFtFzzutVAU9d91TagGzWBhNoMfplwVTns27GOOgv1dn5s
+QSSSmP0hTbPMDlThysKkR9BiOVbBtWGQpV936pPBgyWERGqMqC9xykLdVHv2Vu05
+T0WMwKCAetgtAgMBAAE=
+-----END RSA PUBLIC KEY-----
+	'''
+	f = open('/Users/exodia/Downloads/temp.pub', 'rb')
+	key = RSA.importKey(f.read())
+	print(key.n)
+
 
 
 	
