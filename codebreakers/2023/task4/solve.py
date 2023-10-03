c67e308aefde4df099f4cb4e8f941af434322139
dillon wu <dillonwu999@gmail.com>
2023-10-02 20:53:17 -0700

    solve script

--- codebreakers/2023/task4/solve.py
+++ codebreakers/2023/task4/solve.py
@@ -10,9 +10,9 @@ def sandbox():
 
 def check_hash(inp):
     cmd = [
-        '/bin/zsh',
+        '/bin/bash',
         '-c',
-        f'echo -n {inp} | cryptsetup open --test-passphrase ./myfiles/payload.enc -T1'
+        f'echo -n {inp} | cryptsetup open --test-passphrase ./myfiles/payload.enc',
     ]
     res = subprocess.run(cmd, capture_output=True, text=True)
     return res.stderr
@@ -21,64 +21,59 @@ def check_hash(inp):
     else:
         return res.stderr
 
+def build_list():
+    print("[*] Creating dictionary")
+    alphanum = '0123456789abcdef'
+    f = open('./dic.txt', 'w')
+    hostname = 'overtlycra'
+
+    for i in alphanum:
+        for j in alphanum:
+            for k in alphanum:
+                id = hostname + i + j + k 
+                id_hash = sha1(id.encode()).hexdigest()
+                f.write(id_hash + '\n')
+    f.close()
+
 def solve():
     print("[*] Testing")
     sandbox()
-
-#    password_list = open('./password_list', 'w')
     print("[*] Producing all hashes")
     hostname = 'overtlycra'
-    #alphanum = string.ascii_letters + string.digits
-    alphanum = string.digits
-    #                password_list.write(id_hash + '\n')
-#    password_list.close()
+    alphanum = string.ascii_letters + string.digits
 
-    a = [
-        "dle",
-        "fts",
-        "kes",
-        "mps",
-        "nch",
-        "ned",
-        "nes",
-        "nks",
-        "nny",
-        "ped",
-        "pes",
-        "ppy",
-        "sis",
-        "tch",
-        "ted",
-        "ter",
-        "tes",
-        "vat",
-        "ved",
-        "ven",
-        "ver",
-        "ves",
-        "wls",
-        "wly",
-        "yon"
-    ]
     hostname = 'overtlycra'
     for i in alphanum:
         for j in alphanum:
             for k in alphanum:
-                #id = hostname + chr(i) + chr(j) + chr(k)
-#    for s in a:
-        #id = hostname + i + j + k
                 id = hostname + i + j + k
                 print(f"Trying: {id}")
                 id_hash = sha1(id.encode()).hexdigest()
                 print(f"Trying: {id_hash}")
                 out = check_hash(id_hash)
-                print(out)
+                print(f"Message: {out}")
                 if "No key" not in out:
                     print("FOUND", id)
                     input()
 
+def recover():
+    to_find = '87679d282b7869b3900d61875d0123ad367936e8'
+    str_list = '0123456789abcdef'
+    assert (len(to_find) % 2 == 0)
+    for i in str_list:
+        for j in str_list: 
+            for k in str_list:
+                id = 'overtlycra' + i + j + k
+                id_hash = sha1(id.encode()).hexdigest()
+                print(id_hash)
+                if id_hash == to_find:
+                    print(i + j + k)
+                    input()
+
 def main():
-    solve()
+    #solve()
+    #build_list()
+    recover()
 
 if __name__ == '__main__':
     main()
